2ce4cdfaa45ae11e619541d8c9c8d80edafe327b
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-05-04 11:53:25 +0000

    versao2

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,16 @@
+const cardapio = [
+    {nome: "X-Burder", preco: 18.90, categotia:"Lanche"},
+    {nome: "Batata-frita", preco: 9.50, categoria:"acompamhamento"},
+    {nome:"Suco de Laranja", preco: 7.00, categoria:"bebida"},
+    {nome:"Milkshake", preco: 12.00, categoria:"sobremesa"}
+];
+
+console.log(cardapio);
+
+const pedido = [];
+
+console.log(cardapio[0].nome);
+console.log(cardapio[1].preco);
+console.log(cardapio[2].categoria);
+
 
