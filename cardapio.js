3abe40c323b17b16411c8041df2dbd3f4b7733fd
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-05-05 11:52:59 +0000

    versao4

--- cardapio.js
+++ cardapio.js
@@ -13,4 +13,16 @@ console.log(cardapio[0].nome);
 console.log(cardapio[1].preco);
 console.log(cardapio[2].categoria);
 
+function exibirCardapio(lista) {
+    console.log("===CARDÁPIO===");
+    console.log(`1. ${lista[0].nome} - R$ ${lista[0].preco}`)
+    console.log(`2. ${lista[1].nome} - R$ ${lista[1].preco}`)
+    console.log(`3. ${lista[2].nome} - R$ ${lista[2].preco}`)
+    console.log(`4. ${lista[3].nome} - R$ ${lista[3].preco}`)
+}
+
+functioncalculadorTotal(listaPedido) {
+    lettotal = listaPedido[0] .preco + listaPedido[1] .preco;
+    return total;
+}
 
